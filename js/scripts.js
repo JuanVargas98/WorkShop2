4d27daf142c2f905393ba2f6b33ac615b1727d46
OscarFlorez <66635225+Santiago-Florez@users.noreply.github.com>
2021-09-14 22:47:50 -0500

    Creando archivo js y dandole funcionalidad

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var rowId;
+document.getElementById("petsave-button").onclick = function updateTable(){
+    rowId += 1
+    let petData = {
+        dateRegister: document.getElementById("date-input").value,
+        ownerName: document.getElementById("owner-input").value,
+        petName: document.getElementById("petname-input").value,
+        petMicrochip: document.getElementById("petMicrochip-input").value,
+        petSpecies: document.getElementById("petspecies-input").value,
+        petSize: document.getElementById("petsize-input").value,
+        petSex: document.getElementById("petsex-input").value,
+        petDangerous: document.getElementById("petdangerous-input").value,
+        esterilizacion: document.getElementById("esterilizacion-input").value,
+        petLocation: document.getElementById("petlocation-input").value,
+        petBreed: document.getElementById("petBreed-input").value
+    }
+}
